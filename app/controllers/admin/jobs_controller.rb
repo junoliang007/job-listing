9588d73e85dc8bd386dd366028d706aeea13070e
junoliang <junoliang007@gmail.com>
2017-01-11 19:26:08 +0800

    implement part2

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,6 +1,6 @@
 class Admin::JobsController < ApplicationController
 
-before_action :authenticate_user!, only: [:new, :create, :update, :edit, destroy]
+before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
 before_action :require_is_admin
 
 
